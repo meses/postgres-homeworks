048c41f007ea533361b8c4edf1461c63eb3ff5d6
Artem <akirillov@itps-russia.ru>
2023-04-24 23:53:48 +0500

    Создание БД, таблиц и заполнение данными

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,74 @@
 -- SQL-команды для создания таблиц
+
+
+--Создание БД
+CREATE DATABASE north
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+--Создание последовательности для генерации id сотрудников
+CREATE SEQUENCE public.employees_emp_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+--Создание таблицы employees
+CREATE TABLE public.employees (
+	emp_id serial NOT NULL,
+	first_name varchar NOT NULL,
+	last_name varchar NULL,
+	title varchar NULL,
+	birth_date date NULL,
+	notes text NULL,
+	CONSTRAINT employees_pk PRIMARY KEY (emp_id)
+);
+COMMENT ON TABLE public.employees IS 'Сотрудник';
+
+-- Column comments
+
+COMMENT ON COLUMN public.employees.emp_id IS 'Идентификатор';
+COMMENT ON COLUMN public.employees.first_name IS 'Имя';
+COMMENT ON COLUMN public.employees.last_name IS 'Фамилия';
+COMMENT ON COLUMN public.employees.birth_date IS 'Дата рождения';
+COMMENT ON COLUMN public.employees.notes IS 'Заметки';
+
+--Создание таблицы customers
+CREATE TABLE public.customers (
+	customer_id serial NOT NULL,
+	company_name varchar NULL,
+	contact_name varchar NULL,
+	CONSTRAINT customers_pk PRIMARY KEY (customer_id)
+);
+
+-- Column comments
+
+COMMENT ON COLUMN public.customers.customer_id IS 'Идентификатор';
+COMMENT ON COLUMN public.customers.company_name IS 'Название компании';
+COMMENT ON COLUMN public.customers.contact_name IS 'Имя контактного лица';
+
+--Создание таблицы order
+CREATE TABLE public."order" (
+	order_id int NOT NULL,
+	customer_id varchar NOT NULL,
+	employee_id int NULL,
+	order_date date NULL,
+	ship_city varchar NULL,
+	CONSTRAINT order_pk PRIMARY KEY (order_id),
+	CONSTRAINT customer_id_fk FOREIGN KEY (customer_id) REFERENCES public.customers(customer_id),
+	CONSTRAINT emp_id_fk FOREIGN KEY (employee_id) REFERENCES public.employees(emp_id)
+);
+COMMENT ON TABLE public."order" IS 'Заказ';
+
+-- Column comments
+
+COMMENT ON COLUMN public."order".order_id IS 'Идентификатор';
+COMMENT ON COLUMN public."order".customer_id IS 'Заказчик';
+COMMENT ON COLUMN public."order".employee_id IS 'Сотрудник';
+COMMENT ON COLUMN public."order".order_date IS 'Дата заказа';
+COMMENT ON COLUMN public."order".ship_city IS 'Город доставки';
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,63 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+
+emp_rows = []
+customers_rows = []
+order_rows = []
+def read_emp_file():
+    with open('north_data/employees_data.csv', newline='', encoding='UTF-8') as file:
+        rows = csv.reader(file)
+        for row in rows:
+            row = tuple(row)
+            emp_rows.append(row)
+
+
+def read_customers_file():
+    with open('north_data/customers_data.csv', newline='', encoding='UTF-8') as file:
+        rows = csv.reader(file)
+        for row in rows:
+            row = tuple(row)
+            customers_rows.append(row)
+
+
+def read_order_file():
+    with open('north_data/orders_data.csv', newline='', encoding='UTF-8') as file:
+        rows = csv.reader(file)
+        for row in rows:
+            row = tuple(row)
+            order_rows.append(row)
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password='cnmeses'
+)
+try:
+    with conn:
+        with conn.cursor() as cur:
+            read_emp_file()
+            for row in emp_rows:
+                if row[0] != 'first_name':
+                    cur.execute("INSERT INTO employees (first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s)", row)
+            print('Успех')
+            conn.commit()
+
+            read_customers_file()
+            for row in customers_rows:
+                if row[0] != 'customer_id':
+                    cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", row)
+            print('Успех')
+            conn.commit()
+
+            read_order_file()
+            for row in order_rows:
+                if row[0] != 'order_id':
+                    cur.execute('INSERT INTO "order" VALUES (%s, %s, %s, %s, %s)', row)
+            print('Успех')
+finally:
+    conn.close()
+
+
